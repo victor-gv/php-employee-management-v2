ab61daefd175725d08099e6924221cca8b551111
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-06-21 12:01:02 +0200

    added config files

---
+++ config/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "baseConstants.php";
+
+//CONTROLLERS
+define("CONTROLLERS", BASE_PATH . '/src/controllers/');
+
+//VIEWS
+define("VIEWS", BASE_PATH . '/src/views/');
+
+//MODELS
+define("MODELS", BASE_PATH . '/src/models/');
+
+//RESOURCES
+define("DB", BASE_PATH . '/db/');
---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+
+define('HOST', 'localhost');
+define('DB', 'employees_v2');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
