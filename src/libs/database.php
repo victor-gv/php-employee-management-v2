cf0ea9dc5a2b031778fd1af0f9655560f194588e
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-06-21 12:17:20 +0200

    added database connection

---
+++ src/libs/database.php
@@ -0,0 +1,21 @@
+<?php 
+
+class database {
+    public $connection = "mysql:host=" . HOST . ";"
+    . "dbname=" . DB . ";"
+    . "charset=" . CHARSET . ";";
+
+    public $options = [
+        PDO::ATTR_ERRMODE           =>  PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES  => FALSE,
+    ];
+
+
+    public function getConnection(){
+        try {
+            return new PDO ($this->connection, USER, PASSWORD, $this->options);
+        } catch (PDOException $e) {
+            require_once(VIEWS . "/error/error.php");
+        }
+    } 
+} 
